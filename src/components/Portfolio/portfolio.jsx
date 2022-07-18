0cb2d6aa00295e54e8d05b01719d0a6963e62937
ishantsingla18 <57613954+ishantsingla18@users.noreply.github.com>
2022-07-18 17:06:26 +0530

    added the zoom image functionality in portfolio section

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -11,12 +11,12 @@ function Portfolio() {
             <p>Automation is made easier with Koders. Take a look at the various projects we’ve done</p>
         </div>
         <div className="portfolio-cards">
-            <PortfolioCard imgSrc="/hawa.png" delay="200" />
-            <PortfolioCard imgSrc="/pop-bot.png"  delay="300"  />               
-            <PortfolioCard imgSrc="/Divine.png"  delay="200"  />
-            <PortfolioCard imgSrc="/Machina.png"  delay="300"  />
-            <PortfolioCard imgSrc="/squared.png"  delay="200"  />
-            <PortfolioCard imgSrc="/uc-toolbot.png"  delay="300"  />
+            <PortfolioCard imgSrc={["/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]} delay="200" title="Hawa" classTitle="hawa" position="left" />
+            <PortfolioCard imgSrc={["/pop-bot.png", "/hawa.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" title="Pop Bot" classTitle="pop-bot" position="right" />               
+            <PortfolioCard imgSrc={["/Divine.png", "/hawa.png", "/pop-bot.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="200" title="Divine" classTitle="divine" position="left" />
+            <PortfolioCard imgSrc={["/Machina.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" title="Machina" classTitle="machina" position="right" />
+            <PortfolioCard imgSrc={["/squared.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/uc-toolbot.png"]}  delay="200" title="Squarred" classTitle="squarred" position="left" />
+            <PortfolioCard imgSrc={["/uc-toolbot.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png"]}  delay="300" title="Uc Toolbot" classTitle="uc-toolbot" position="right" />
         </div>
     </div>
   )
