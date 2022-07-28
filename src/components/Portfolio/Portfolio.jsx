8e461a593b4b190c1e8ddb95210b66f213c9a64f
ishantsingla18 <57613954+ishantsingla18@users.noreply.github.com>
2022-07-28 17:29:02 +0530

    added content to the portfolio section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PortfolioCard from '../PortfolioCard/PortfolioCard';
 import './Portfolio.css';
+import { arr } from '../../helper';
 
 function Portfolio() { 
   
@@ -19,18 +20,23 @@ function Portfolio() {
             <p>Automation is made easier with Koders. Take a look at the various projects we’ve done</p>
         </div>
         <div className="portfolio-cards">
-            <PortfolioCard hidden={false} imgSrc={["/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]} delay="200" title="Hawa" classTitle="hawa" position="left" />
-            <PortfolioCard hidden={false} imgSrc={["/pop-bot.png", "/hawa.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" title="Pop Bot" classTitle="pop-bot" position="right" />               
-            <PortfolioCard hidden={false} imgSrc={["/Divine.png", "/hawa.png", "/pop-bot.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="200" title="Divine" classTitle="divine" position="left" />
-            <PortfolioCard hidden={false} imgSrc={["/Machina.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" title="Machina" classTitle="machina" position="right" />
-            <PortfolioCard hidden={false} imgSrc={["/squared.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/uc-toolbot.png"]}  delay="200" title="Squarred" classTitle="squarred" position="left" />
-            <PortfolioCard hidden={false} imgSrc={["/uc-toolbot.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png"]}  delay="300" title="Uc Toolbot" classTitle="uc-toolbot" position="right" />
-            <PortfolioCard hidden={true} imgSrc={["/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]} delay="200" title="Hawa" classTitle="hawa" position="left" />
-            <PortfolioCard hidden={true} imgSrc={["/pop-bot.png", "/hawa.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" title="Pop Bot" classTitle="pop-bot" position="right" />               
-            <PortfolioCard hidden={true} imgSrc={["/Divine.png", "/hawa.png", "/pop-bot.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="200" title="Divine" classTitle="divine" position="left" />
-            <PortfolioCard hidden={true} imgSrc={["/Machina.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" title="Machina" classTitle="machina" position="right" />
-            <PortfolioCard hidden={true} imgSrc={["/squared.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/uc-toolbot.png"]}  delay="200" title="Squarred" classTitle="squarred" position="left" />
-            <PortfolioCard hidden={true} imgSrc={["/uc-toolbot.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png"]}  delay="300" title="Uc Toolbot" classTitle="uc-toolbot" position="right" />
+          {
+            arr.map(item => (
+              
+              <PortfolioCard hidden={item.hidden} imgSrc={item.imgSrc} delay={item.delay} classTitle={item.classTitle} position={item.position} />
+            ))
+          }
+            {/* <PortfolioCard hidden={false} imgSrc={["/pop-bot.png", "/hawa.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" classTitle="pop-bot" position="right" />               
+            <PortfolioCard hidden={false} imgSrc={["/Divine.png", "/hawa.png", "/pop-bot.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="200" classTitle="divine" position="left" />
+            <PortfolioCard hidden={false} imgSrc={["/Machina.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" classTitle="machina" position="right" />
+            <PortfolioCard hidden={false} imgSrc={["/squared.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/uc-toolbot.png"]}  delay="200" classTitle="squarred" position="left" />
+            <PortfolioCard hidden={false} imgSrc={["/uc-toolbot.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png"]}  delay="300" classTitle="uc-toolbot" position="right" />
+            <PortfolioCard hidden={true} imgSrc={["/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="200" classTitle="hawa" position="left" />
+            <PortfolioCard hidden={true} imgSrc={["/pop-bot.png", "/hawa.png", "/Divine.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" classTitle="pop-bot" position="right" />               
+            <PortfolioCard hidden={true} imgSrc={["/Divine.png", "/hawa.png", "/pop-bot.png", "/Machina.png", "/squared.png", "/uc-toolbot.png"]}  delay="200" classTitle="divine" position="left" />
+            <PortfolioCard hidden={true} imgSrc={["/Machina.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/squared.png", "/uc-toolbot.png"]}  delay="300" classTitle="machina" position="right" />
+            <PortfolioCard hidden={true} imgSrc={["/squared.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/uc-toolbot.png"]}  delay="200" classTitle="squarred" position="left" />
+            <PortfolioCard hidden={true} imgSrc={["/uc-toolbot.png", "/hawa.png", "/pop-bot.png", "/Divine.png", "/Machina.png", "/squared.png"]}  delay="300" classTitle="uc-toolbot" position="right" /> */}
         </div>
         <button className='portfolio-btn' onClick={handleClick}>view all</button>
     </div>
---
+++ src/helper.js
@@ -0,0 +1,175 @@
+import kt1 from './Assests/kyro-tools/kt1.png';
+import kt2 from './Assests/kyro-tools/kt2.png';
+import kt3 from './Assests/kyro-tools/kt3.png';
+import kt4 from './Assests/kyro-tools/kt4.png';
+import kt5 from './Assests/kyro-tools/kt5.png';
+import kt6 from './Assests/kyro-tools/kt6.png';
+import jt1 from './Assests/jupiter-toolbox/jt1.png';
+import jt2 from './Assests/jupiter-toolbox/jt2.png';
+import jt3 from './Assests/jupiter-toolbox/jt3.png';
+import jt4 from './Assests/jupiter-toolbox/jt4.png';
+import jt5 from './Assests/jupiter-toolbox/jt5.png';
+import jt6 from './Assests/jupiter-toolbox/jt6.png';
+import divine1 from './Assests/divine/divine1.png';
+import divine2 from './Assests/divine/divine2.png';
+import divine3 from './Assests/divine/divine3.png';
+import divine4 from './Assests/divine/divine4.png';
+import divine5 from './Assests/divine/divine5.png';
+import divine6 from './Assests/divine/divine6.png';
+import instaride1 from './Assests/instaride/instaride1.png';
+import instaride2 from './Assests/instaride/instaride2.png';
+import instaride3 from './Assests/instaride/instaride3.png';
+import instaride4 from './Assests/instaride/instaride4.png';
+import genesis1 from './Assests/genesis/genesis1.png';
+import genesis2 from './Assests/genesis/genesis2.png';
+import genesis3 from './Assests/genesis/genesis3.png';
+import genesis4 from './Assests/genesis/genesis4.png';
+import genesis5 from './Assests/genesis/genesis5.png';
+import genesis6 from './Assests/genesis/genesis6.png';
+import spacebot1 from './Assests/spacebot/spacebot1.png';
+import spacebot2 from './Assests/spacebot/spacebot2.png';
+import spacebot3 from './Assests/spacebot/spacebot3.png';
+import spacebot4 from './Assests/spacebot/spacebot4.png';
+import pluto1 from './Assests/pluto/pluto1.png';
+import pluto2 from './Assests/pluto/pluto2.png';
+import pluto3 from './Assests/pluto/pluto3.png';
+import pluto4 from './Assests/pluto/pluto4.png';
+import hawa1 from './Assests/hawa/hawa1.jpg';
+import hawa2 from './Assests/hawa/hawa2.jpg';
+import hawa3 from './Assests/hawa/hawa3.jpg';
+import hawa4 from './Assests/hawa/hawa4.jpg';
+import hawa5 from './Assests/hawa/hawa5.jpg';
+import hawa6 from './Assests/hawa/hawa6.jpg';
+import uc1 from './Assests/uc-toolbot/uc1.jpg';
+import uc2 from './Assests/uc-toolbot/uc2.jpg';
+import uc3 from './Assests/uc-toolbot/uc3.jpg';
+import uc4 from './Assests/uc-toolbot/uc4.jpg';
+import uc5 from './Assests/uc-toolbot/uc5.jpg';
+import uc6 from './Assests/uc-toolbot/uc6.jpg';
+import uc7 from './Assests/uc-toolbot/uc7.jpg';
+import uc8 from './Assests/uc-toolbot/uc8.jpg';
+import squared1 from './Assests/squared/squared1.jpg';
+import squared2 from './Assests/squared/squared2.jpg';
+import squared3 from './Assests/squared/squared3.jpg';
+import squared4 from './Assests/squared/squared4.jpg';
+import arc1 from './Assests/arc/arc1.png';
+import arc2 from './Assests/arc/arc2.png';
+import arc3 from './Assests/arc/arc3.png';
+import arc4 from './Assests/arc/arc4.png';
+import aio1 from './Assests/aio-world/aio1.jpg';
+import aio2 from './Assests/aio-world/aio2.jpg';
+import aio3 from './Assests/aio-world/aio3.jpg';
+import aio4 from './Assests/aio-world/aio4.jpg';
+import aio5 from './Assests/aio-world/aio5.jpg';
+import argon1 from './Assests/argon/argon1.png';
+import argon2 from './Assests/argon/argon2.png';
+import argon3 from './Assests/argon/argon3.png';
+import argon4 from './Assests/argon/argon4.png';
+import epon1 from './Assests/epon/epon1.png';
+import epon2 from './Assests/epon/epon2.png';
+import epon3 from './Assests/epon/epon3.png';
+import epon4 from './Assests/epon/epon4.png';
+import popbot1 from './Assests/popbot/popbot1.png';
+import popbot2 from './Assests/popbot/popbot2.png';
+
+export const arr = [
+    {
+        imgSrc: [kt1, kt2, kt3, kt5, kt6],
+        hidden: false,
+        delay: '200',
+        classTitle: "kyro-tools",
+        position: "left"
+    },
+    {
+        imgSrc: [divine3, divine2, divine1, divine4, divine5, divine6],
+        hidden: false,
+        delay: '300',
+        classTitle: "divine",
+        position: "right"
+    },
+    {
+        imgSrc: [hawa1, hawa2, hawa3, hawa4, hawa5, hawa6],
+        hidden: false,
+        delay: '200',
+        classTitle: "hawa",
+        position: "left"
+    },
+    {
+        imgSrc: [spacebot4, spacebot1, spacebot2, spacebot3],
+        hidden: false,
+        delay: '300',
+        classTitle: "spacebot",
+        position: "right"
+    },
+    
+    {
+        imgSrc: [genesis1, genesis2, genesis3, genesis4, genesis5, genesis6],
+        hidden: false,
+        delay: '200',
+        classTitle: "genesis",
+        position: "left"
+    },
+    {
+        imgSrc: [jt1, jt2, jt3, jt4, jt5, jt6],
+        hidden: false,
+        delay: '300',
+        classTitle: "jupiter-toolbox",
+        position: "right"
+    },
+    {
+        imgSrc: [pluto3, pluto1, pluto2, pluto4],
+        hidden: true,
+        delay: '200',
+        classTitle: "pluto",
+        position: "left"
+    },
+    {
+        imgSrc: [uc1, uc2, uc3, uc4, uc5, uc6, uc7, uc8],
+        hidden: true,
+        delay: '300',
+        classTitle: "uc-toolbot",
+        position: "right"
+    },
+    {
+        imgSrc: [squared3, squared1, squared2, squared4],
+        hidden: true,
+        delay: '200',
+        classTitle: "squared",
+        position: "left"
+    },
+    {
+        imgSrc: [arc1, arc2, arc3, arc4],
+        hidden: true,
+        delay: '300',
+        classTitle: "arc",
+        position: "right"
+    },
+    {
+        imgSrc: [aio1, aio2, aio3, aio4, aio5],
+        hidden: true,
+        delay: '200',
+        classTitle: "aio-world",
+        position: "left"
+    },
+    {
+        imgSrc: [argon1, argon2, argon3, argon4],
+        hidden: true,
+        delay: '300',
+        classTitle: "argon",
+        position: "right"
+    },
+    {
+        imgSrc: [epon1, epon2, epon3, epon4],
+        hidden: true,
+        delay: '200',
+        classTitle: "epon",
+        position: "left"
+    },
+    {
+        imgSrc: [popbot1, popbot2],
+        hidden: true,
+        delay: '300',
+        classTitle: "popbot",
+        position: "right"
+    }
+]
