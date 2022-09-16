0577b6b53b45952d24b37b4ebed0646f418715be
Jack1998 <yaduvanshiabhi1998@gmail.com>
2022-09-16 11:23:54 +0530

    add cookie consent functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 import About from ".././src/components/About/About";
 import TitlePage from ".././src/components/TitlePage/TitlePage";
 import Footer from "./components/Footer/Footer";
 import KeyValues from "./components/KeyValues/KeyValues";
 import Clients from ".././src/components/Clients/Clients";
 import Portfolio from ".././src/components/Portfolio/Portfolio";
-import SplashScreen from './components/SplashScreen/SplashScreen';
-
+import SplashScreen from "./components/SplashScreen/SplashScreen";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
-
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -22,22 +21,48 @@ function App() {
 
   return (
     <div className="App">
-      {
-        loading ? (
-          <SplashScreen />
-        ) : ( <>
-          <TitlePage  />
+      {loading ? (
+        <SplashScreen />
+      ) : (
+        <>
+          <TitlePage />
           <About />
           <KeyValues />
           <Clients />
           <Portfolio />
           <Footer />
-          </>
-        )
-      }
+          <CookieConsent
+            buttonText="Accept All"
+            enableDeclineButton
+            setDeclineCookie={false}
+            declineButtonText="Decline"
+            onAccept={() => {}}
+            style={{
+              background: "#1d2c48",
+              fontSize: "14px",
+              textAlign: "left",
+            }}
+            buttonStyle={{
+              padding: "10px 20px",
+              border: "1px solid #ffffff",
+              color: "#ffffff",
+              background: "transparent",
+            }}
+            declineButtonStyle={{
+              border: "1px solid gray",
+              background: "transparent",
+              padding: "10px 20px",
+              color: "gray",
+            }}
+          >
+            We use cookies to enhance your browsing experience, serve
+            personalized ads or content, and analyze our traffic. By clicking
+            'Accept All', you consent to our use of cookies.
+          </CookieConsent>
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
-
